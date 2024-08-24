06e4c396f6cce9f79cb4c3708175e10ff893f639
Hari Chandana <gottuparthiharichandana@gmail.com>
2024-08-24 13:05:55 +0530

    implemented loading feature

--- script.js
+++ script.js
@@ -16,56 +16,91 @@ function debounceEffect(delay) {
   };
 }
 
+async function getTrailer(imdbId) {
+  const url = `https://ott-details.p.rapidapi.com/getadditionalDetails?imdbid=${imdbId}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "3ca895565emshe3431b6ea04373ep1d4dbbjsncb4ccf32ba06",
+      "x-rapidapi-host": "ott-details.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log(data);
+    if (data.trailerUrl.length === 0) return "not found";
+    return data.trailerUrl[0];
+  } catch (e) {
+    return "not found";
+  }
+}
+
 async function showMoreDetails(movieTitle) {
   const url = `https://omdbapi.com/?i=${imdbId}&apikey=${apiKey}&t=${movieTitle}`;
   const response = await fetch(url);
   const data = await response.json();
+  const loadingText = document.createElement("h2");
+  loadingText.innerText = "Loading...";
+  loadingText.className = "loading";
+  body.appendChild(loadingText);
 
-  const modalBg = document.createElement("div");
-  modalBg.className = "modal-bg";
-  modalBg.style.display = "block";
+  setTimeout(async () => {
+    body.removeChild(loadingText);
+    const trailerSrc = await getTrailer(data.imdbID);
 
-  const card = document.createElement("div");
-  const poster = document.createElement("img");
-  const title = document.createElement("h3");
-  const list = document.createElement("ul");
-  const button = document.createElement("button");
+    const modalBg = document.createElement("div");
+    modalBg.className = "modal-bg";
+    modalBg.style.display = "block";
 
-  card.className = "modal-card";
-  button.innerText = "Close";
-  button.className = "show-details";
+    const card = document.createElement("div");
+    const poster = document.createElement("img");
+    const title = document.createElement("h3");
+    const linkToImdb = document.createElement("a");
+    const list = document.createElement("ul");
+    const button = document.createElement("button");
 
-  for (const key in data) {
-    if (
-      key === "Poster" ||
-      key === "Title" ||
-      key === "Ratings" ||
-      key === "Response" ||
-      key === "imdbID" ||
-      data[key] === "N/A"
-    )
-      continue;
-    const li = document.createElement("li");
-    li.innerText = `${key}: ${data[key]}`;
-    list.appendChild(li);
-  }
+    card.className = "modal-card";
+    button.innerText = "Close";
+    button.className = "show-details";
+    linkToImdb.href = trailerSrc;
+    linkToImdb.target = "_blank";
+    linkToImdb.innerText = `${data.Title} (👈Trailer Link!)`;
 
-  poster.src = data.Poster;
-  title.innerText = data.Title;
+    for (const key in data) {
+      if (
+        key === "Poster" ||
+        key === "Title" ||
+        key === "Ratings" ||
+        key === "Response" ||
+        key === "imdbID" ||
+        data[key] === "N/A"
+      )
+        continue;
+      const li = document.createElement("li");
+      li.innerText = `${key}: ${data[key]}`;
+      list.appendChild(li);
+    }
 
-  card.appendChild(poster);
-  card.appendChild(title);
-  card.appendChild(list);
-  card.appendChild(button);
+    poster.src = data.Poster;
+    if (trailerSrc !== "not found") title.appendChild(linkToImdb);
+    else title.innerText = data.Title;
 
-  modalBg.appendChild(card);
+    card.appendChild(poster);
+    card.appendChild(title);
+    card.appendChild(list);
+    card.appendChild(button);
 
-  body.appendChild(modalBg);
-  console.log(data);
+    modalBg.appendChild(card);
 
-  button.addEventListener("click", () => {
-    body.removeChild(modalBg);
-  });
+    body.appendChild(modalBg);
+    console.log(data);
+
+    button.addEventListener("click", () => {
+      body.removeChild(modalBg);
+    });
+  }, 2000);
 }
 
 async function getMovies(searchTitle = "marvel") {
